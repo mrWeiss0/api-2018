982a6b8fa62bd0edfba49168b7e4c02c83d2e97f
mrWeiss <giorgio2197@gmail.com>
2018-08-07 22:14:19 +0200

    Add struct tm

--- ndtm.c
+++ ndtm.c
@@ -14,12 +14,17 @@
 
 #define BUFSZ 512
 
+/* Machine settings */
+struct tm{
+};
+
+void tm_init   (struct tm*);
+void tm_destroy(struct tm*);
+
 /* Functions to parse each section of the input */
-void f_tr (char*);
-void f_acc(char*);
-void f_max(char*);
-void f_run(char*);
-void  (* const parse[])(char*) = {f_tr, f_acc, f_max, f_run};
+typedef void parse_funct(char*, struct tm*);
+parse_funct f_tr, f_acc, f_max, f_run,
+    * const parse[] = {f_tr, f_acc, f_max, f_run};
 
 /*
  * The program takes input divided in 4 sections: tr, acc, max, run.
@@ -34,6 +39,8 @@ int main(){
     size_t  buf_sz,
             lbuf_sz =  0;
     int     st      = -1;    /* Current section state */
+    struct tm tm;
+    tm_init(&tm);
     while(!feof(stdin)){
         /*
          * Read characters from input to buf and increase lbuf
@@ -62,33 +69,50 @@ int main(){
          * the end of file is reached
          */
         if(lbuf && (lbuf[lbuf_sz - 1] == '\n' || feof(stdin))){
-            parse[st](lbuf);
+            parse[st](lbuf, &tm);
             free(lbuf);
             lbuf = NULL;
             lbuf_sz = 0;
         }
     }
+    tm_destroy(&tm);
     return EXIT_SUCCESS;
 }
 
-void f_tr(char *s){
+/******************** TM init and delete ********************/
+
+void tm_init(struct tm *tm){
+    (void)tm;
+}
+
+void tm_destroy(struct tm *tm){
+    (void)tm;
+}
+
+/******************** Parser functions ********************/
+
+void f_tr(char *s, struct tm *tm){
     unsigned int in_st, out_st;
     char in_ch, out_ch;
     char mv;
+    (void)tm;
     sscanf(s, "%u %c %c %c %u",  &in_st, &in_ch, &out_ch, &mv, &out_st);
     printf(   "%u %c %c %c %u\n", in_st,  in_ch,  out_ch,  mv,  out_st);
 }
 
-void f_acc(char *s){
+void f_acc(char *s, struct tm *tm){
     unsigned int st = atoi(s);
+    (void)tm;
     printf("%u\n", st);
 }
 
-void f_max(char *s){
+void f_max(char *s, struct tm *tm){
     unsigned int max = atoi(s);
+    (void)tm;
     printf("%u\n", max);
 }
 
-void f_run(char *s){
+void f_run(char *s, struct tm *tm){
+    (void)tm;
     fputs(s, stdout);
 }
